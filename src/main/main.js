22ffe0ecc6eb6811f74ece1f155ebc2beccb20ea
ChilenoSika <jaum97@yahoo.com.br>
2018-11-01 12:57:43 -0300

    Simple promise based repo display app

--- src/main/main.js
+++ src/main/main.js
@@ -1,13 +1,4 @@
 /*
-xhr.open('GET', 'https://api.github.com/users/jaum97');
-xhr.send(null);
-
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        console.log(JSON.parse(xhr.responseText));
-    }
-}
-*/
 const myPromise = function () {
     return new Promise(function (resolve, reject) {
         const xhr = new XMLHttpRequest();
@@ -33,3 +24,87 @@ myPromise()
     .catch(function (error) {
         console.warn(error);
     });
+    */
+/*
+axios.get('https://api.github.com/users/jaum97')
+ .then(function (response) {
+     console.log(response.data.avatar_url);
+ })
+ .catch(function (error) {
+     console.warn(error);
+ });
+ */
+function checaIdade(idade) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            return idade > 17 ? resolve() : reject();
+        }, 2000);
+    });
+}
+
+checaIdade(17)
+    .then(function () {
+        console.log('maior que 18');
+    })
+    .catch(function () {
+        console.log('menor que 18');
+    });
+
+function renderRepos(repositories) {
+    while (searchul.firstChild) searchul.removeChild(searchul.firstChild)
+    for (repo of repositories) {
+        const reponli = document.createElement('li');
+        const reponame = document.createTextNode(repo.name);
+        reponli.appendChild(reponame);
+        searchul.appendChild(reponli);
+    }
+}
+
+function renderLoading(loading) {
+    searchul.innerHTML = "";
+    const textElement = document.createTextNode('loading...');
+    const loadingElement = document.createElement('li');
+    loadingElement.appendChild(textElement);
+    searchul.appendChild(loadingElement);
+}
+
+function renderError(loading) {
+    searchul.innerHTML = "";
+    const textElement = document.createTextNode('Error!');
+    const errorElement = document.createElement('li');
+    errorElement.style.color = "#F00";
+    errorElement.appendChild(textElement);
+    searchul.appendChild(errorElement);
+}
+
+const app = document.querySelector('#app');
+const searchul = document.createElement('ul');
+const searchinput = document.createElement('input');
+searchinput.name = 'nome';
+searchinput.type = 'text';
+searchinput.placeholder = 'search repo';
+app.appendChild(searchinput);
+
+const searchbutton = document.createElement('button');
+const buttontext = document.createTextNode('search');
+searchbutton.appendChild(buttontext);
+searchbutton.onclick = function () {
+    const usertosearch = searchinput.value;
+    if (!usertosearch) return;
+
+    renderLoading();
+
+    axios.get('https://api.github.com/users/' + usertosearch + '/repos')
+        .then(function (response) {
+            renderRepos(response.data);
+        })
+        .catch(function (error) {
+            renderError();
+            console.log(error);
+        });
+}
+
+app.appendChild(searchbutton);
+app.appendChild(searchul);
+
+
